70ecde2d23b4c01d746a072e8c9e86ca82e4a4d5
ikafly144 <support@sabafly.net>
2023-01-16 20:39:20 +0900

    タイトルと説明

--- src/pages/index.js
+++ src/pages/index.js
@@ -42,8 +42,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Welcome`}
+      description="Official gobot page">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
